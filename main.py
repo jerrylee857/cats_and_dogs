84fdb8d9cdf07d65271196661e65ab4f44488634
11112 <1111@.com>
2023-11-11 00:28:44 +0800

    modify the main.py to use gpu cuda only

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
 
-import os
+# 检查CUDA是否可用
+if not torch.cuda.is_available():
+    raise RuntimeError("CUDA is not available. Please run this code on a machine with CUDA.")
 
-# 简化后的数据预处理和增强
+# 数据预处理和增强
 transform = transforms.Compose([
     transforms.RandomResizedCrop(128),
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
-
 
 # 训练集和验证集
 train_dataset = datasets.ImageFolder(root='cats_and_dogs_train', transform=transform)
@@ -23,7 +24,7 @@ valid_dataset = datasets.ImageFolder(root='cats_and_dogs_valid', transform=trans
 train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
 valid_loader = DataLoader(valid_dataset, batch_size=128, shuffle=False)
 
-#模型定义
+# 模型定义
 class ImprovedNet(nn.Module):
     def __init__(self):
         super(ImprovedNet, self).__init__()
@@ -39,10 +40,10 @@ class ImprovedNet(nn.Module):
         self.bn5 = nn.BatchNorm2d(512)
         self.conv6 = nn.Conv2d(512, 1024, 3, padding=1)
         self.bn6 = nn.BatchNorm2d(1024)
-        self.conv7 = nn.Conv2d(1024, 2048, 3, padding=1)  # 新增的卷积层
-        self.bn7 = nn.BatchNorm2d(2048)                  # 新增的批标准化层
+        self.conv7 = nn.Conv2d(1024, 2048, 3, padding=1)
+        self.bn7 = nn.BatchNorm2d(2048)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(2048 * 1 * 1, 2048)  # 修改为2048 * 1 * 1
+        self.fc1 = nn.Linear(2048 * 1 * 1, 2048)
         self.fc2 = nn.Linear(2048, 1024)
         self.fc3 = nn.Linear(1024, 512)
         self.fc4 = nn.Linear(512, 128)
@@ -56,7 +57,7 @@ class ImprovedNet(nn.Module):
         x = self.pool(self.bn4(F.relu(self.conv4(x))))
         x = self.pool(self.bn5(F.relu(self.conv5(x))))
         x = self.pool(self.bn6(F.relu(self.conv6(x))))
-        x = self.pool(self.bn7(F.relu(self.conv7(x))))  # 新增的卷积层和批标准化
+        x = self.pool(self.bn7(F.relu(self.conv7(x))))
         x = x.view(x.size(0), -1)
         x = self.dropout(F.relu(self.fc1(x)))
         x = self.dropout(F.relu(self.fc2(x)))
@@ -65,25 +66,24 @@ class ImprovedNet(nn.Module):
         x = self.fc5(x)
         return x
 
-
 # 初始化权重
 def weights_init(m):
     if isinstance(m, nn.Conv2d):
         nn.init.kaiming_normal_(m.weight.data)
 
-# 确定使用的设备
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# 设备设置
+device = torch.device("cuda:0")
 
-# 将模型移动到指定设备并初始化权重
+# 模型实例化并应用权重初始化
 model = ImprovedNet().to(device)
 model.apply(weights_init)
 
-# 初始化优化器和损失函数
+# 优化器和损失函数
 optimizer = optim.Adam(model.parameters(), lr=0.0005, weight_decay=1e-5)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=3, factor=0.1)
 criterion = nn.CrossEntropyLoss()
 
-# 训练和验证
+# 训练和验证过程
 best_valid_acc = 0.0
 for epoch in range(70):
     model.train()
@@ -105,7 +105,6 @@ for epoch in range(70):
     train_acc = 100 * correct_train / total_train
     scheduler.step(train_loss)
 
-    # 验证模型
     model.eval()
     valid_loss = 0.0
     correct_valid = 0
@@ -126,4 +125,4 @@ for epoch in range(70):
     
     if valid_acc > best_valid_acc:
         best_valid_acc = valid_acc
-        torch.save(model.state_dict(), 'best_model.pth')
+        torch.save(model.state_dict(), 'model.pth')
